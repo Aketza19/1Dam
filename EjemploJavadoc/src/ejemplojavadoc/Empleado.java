739e68e4dc2f8ae8f9809e85b7c5bedba8b57f93
Aketza <Aketza@AKETZA-PC>
2020-05-12 10:57:07 +0200

    Generar javadoc

--- EjemploJavadoc/src/ejemplojavadoc/Empleado.java
+++ EjemploJavadoc/src/ejemplojavadoc/Empleado.java
@@ -1,19 +1,42 @@
 package ejemplojavadoc;
 
+/**
+ * 
+ * @author Aketza
+ * @version 1_2020
+ * @since 12/05/2020
+ * 
+ *
+ */
 public class Empleado {
 	private String nombre;
 	private String apellido;
 	private double salario; 
-
+/**
+ * 
+ * @param nombre 
+ * @param apellido
+ * @param salario
+ */
 
 public Empleado(String nombre, String apellido, double salario) {
 	this.nombre = nombre;
 	this.apellido = apellido;
 	this.salario = salario;
 }
+/**
+ * @see 
+ * @param subida - cantidad que se sube
+ */
+
 public void subirsalario (double subida) {
 	salario=salario+subida;
 }
+
+/**
+ * 
+ * @return true si nombre no esta vacio
+ */
 
 private boolean comprobar() {
 	if (nombre.equals("")) {
@@ -21,4 +44,5 @@ private boolean comprobar() {
 	}
 	return true;
 }
+
 }
